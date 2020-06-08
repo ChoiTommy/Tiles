0a966474e4371103a427c7394bf9e29379cef8b9
Kaito <tommychoiht@gmail.com>
2020-06-08 21:24:06 +0800

    added fingerprint authentication

--- app/src/main/java/com/kaito/tiles/MainActivity.kt
+++ app/src/main/java/com/kaito/tiles/MainActivity.kt
@@ -2,34 +2,34 @@ package com.kaito.tiles
 
 //https://github.com/stedi-akk/PowerOffClick
 import android.app.Activity
+import android.media.AudioManager
 import android.os.Bundle
 import android.widget.Toast
-
 
 class MainActivity : Activity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread(Runnable {
-            try {
+            /*try {
                 startActivity(packageManager.getLaunchIntentForPackage("org.thunderdog.challegram"))
             } catch (e: Exception) {
                 Toast.makeText(applicationContext, "Telegram X not installed.\n$e", Toast.LENGTH_SHORT).show()
             }
 
-            /*try {
-                    Process process = Runtime.getRuntime().exec("su");
-                    DataOutputStream os = new DataOutputStream(process.getOutputStream());
-                    os.writeBytes("input keyevent 26" + "\n");
-                    os.writeBytes("exit\n");
-                    os.flush();//https://stackoverflow.com/questions/33529933/how-to-run-root-commands-from-android-application
-                    if (process.waitFor() != 0)
-                        Toast.makeText(MainActivity.this, "Failed.", Toast.LENGTH_SHORT).show();
-                } catch (IOException ex) {
-                    Toast.makeText(MainActivity.this, "Device does not have root access.", Toast.LENGTH_SHORT).show();
-                } catch (InterruptedException ex) {
-                    Toast.makeText(MainActivity.this, "Failed.", Toast.LENGTH_SHORT).show();
-                }*/
+            try {
+                val process = Runtime.getRuntime().exec("su")
+                val os = DataOutputStream(process.outputStream)
+                os.writeBytes("input keyevent 26" + "\n")
+                os.writeBytes("exit\n")
+                os.flush() //https://stackoverflow.com/questions/33529933/how-to-run-root-commands-from-android-application
+            } catch (e: Exception) {
+                val toast = Toast.makeText(this, "failed", Toast.LENGTH_LONG)
+                toast.show()
+            }*/
+            val am = this.getSystemService(AUDIO_SERVICE) as AudioManager
+            am.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI)
         }).start()
+        Toast.makeText(this, "Here to protect your hardware volume buttons", Toast.LENGTH_LONG).show()
         finish()
     }
 }
--- app/src/main/java/com/kaito/tiles/PanelTileService.kt
+++ app/src/main/java/com/kaito/tiles/PanelTileService.kt
@@ -1,21 +1,74 @@
 package com.kaito.tiles
-
-import android.media.AudioManager
+//https://www.youtube.com/watch?v=tXHWyt8g5jc
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.CancellationSignal
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
+import android.widget.Toast
 
 class PanelTileService : TileService() {
 
+    private var cancellationSignal: CancellationSignal? = null
+    private val authenticationCallback: BiometricPrompt.AuthenticationCallback
+        get() =
+            object : BiometricPrompt.AuthenticationCallback(){
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                    super.onAuthenticationError(errorCode, errString)
+                    notify("Authentication error: $errString")
+                }
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                    super.onAuthenticationSucceeded(result)
+                    notify("Your fingerprint data will be sent back to my secret server. Good luck!")
+                    qsTile.state = if (qsTile.state == Tile.STATE_ACTIVE) Tile.STATE_INACTIVE else Tile.STATE_ACTIVE
+                    qsTile.updateTile()
+                }
+            }
+
     override fun onClick() {
-        qsTile.state = Tile.STATE_INACTIVE
-        qsTile.updateTile()
-        val am = this.getSystemService(AUDIO_SERVICE) as AudioManager
-        am.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI)
+        val biometricPrompt = BiometricPrompt.Builder(this)
+                .setTitle("Give me your fingerprint")
+                .setDescription("Place your finger on the scanner.")
+                .setNegativeButton("Cancel", this.mainExecutor, DialogInterface.OnClickListener{ _, _ ->
+                    notify("Authentication cancelled.")
+                }).build()
+        biometricPrompt.authenticate(getCancellationSignal(), mainExecutor, authenticationCallback)
     }
 
     override fun onTileAdded() {
         super.onTileAdded()
+        checkBiometricSupport()
         qsTile.state = Tile.STATE_INACTIVE
         qsTile.updateTile()
+    }
+
+    private fun getCancellationSignal(): CancellationSignal{
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            notify("Authentication was cancelled by the user.")
+        }
+        return cancellationSignal as CancellationSignal
+    }
+
+    private fun checkBiometricSupport(): Boolean{
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+        if (!keyguardManager.isKeyguardSecure){
+            notify("Biometric authentication has not been enabled in settings.")
+            return false
+        }
+
+        /*if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.USE_BIOMETRIC) != PackageManager.PERMISSION_GRANTED) {
+            notify("Biometric authentication permission is not enabled.")
+            return false
+        }*/
+        return if (packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) true else true
+    }
+
+    private fun notify(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
